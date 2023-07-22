4564b950e8347f85fd5c346eb7e982819a128490
divinetara777 <125673326+taratavangar7@users.noreply.github.com>
2023-07-22 20:46:55 +0330

    final JS & CSS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const startCells = [
     "", "", "", "", "", "", "", "", ""
 ]
 let go = "circle"
-infoDisplay.textContent= "Circle goes first"
+infoDisplay.textContent = "Circle goes first"
 
 function createBoard() {
     startCells.forEach((_cell, index) => {
@@ -23,10 +23,47 @@ function addGo(e) {
     goDisplay.classList.add(go)
     e.target.append(goDisplay)
 
-    go = go === "circle" ? "cross" : "circle"  
+    go = go === "circle" ? "cross" : "circle"
     // if go equals the string circle, we wanna change it to cross. otherwise if t's not, we wanna put circle
     // infoDisplay.textContent = "it is now" + go " 's go."
-    e.target.removeEventListener("click" , addGo)
+    e.target.removeEventListener("click", addGo)
     checkScore()
 }
 
+function checkScore() {
+    const allSquares = document.querySelectorAll(".square")
+    const winningCombos = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
+
+    winningCombos.forEach(array => {
+       const circleWins = array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('circle'))
+
+            if (circleWins) {
+                infoDisplay.textContent = "Circle Wins!"
+                allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                return
+
+            }
+    })
+
+    winningCombos.forEach(array => {
+        const crossWins = array.every(cell =>
+             allSquares[cell].firstChild?.classList.contains('cross'))
+ 
+             if (crossWins) {
+                 infoDisplay.textContent = "Cross Wins!"
+                 allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+                 return
+ 
+             }
+     })
+}
